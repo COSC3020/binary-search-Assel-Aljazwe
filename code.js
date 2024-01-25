944e806b3fcc0c21dda54d1c8cf942e0c0ea83cb
Assel-Aljazwe <157559559+Assel-Aljazwe@users.noreply.github.com>
2024-01-25 11:59:45 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,18 @@
 function binarySearch(list, element) {
+    let start = 0;
+    let end = list.length - 1;
+
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+
+        if (list[middle] === element) {
+            return middle;
+        } else if (list[middle] < element) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+
     return -1;
 }
